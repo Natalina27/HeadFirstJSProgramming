91a2cd8726078d23e3bca6d2d4383114d4af3f2a
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-23 20:34:48 +0100

    added code 'Test for jokes ' and 'Movie nights'

--- 01_capture.js
+++ 01_capture.js
@@ -11,3 +11,24 @@ while (count > 0){
     console.log(' Take one down, pass it around, ');
     count--;
 }
+////////////////////////////////////////////////////////////////////
+// Test for jokes
+let joke = 'JavaScript walked into a bar....';
+let toldJoke = "false";
+let $punchline = 'Better watch out for those semi-colons.';
+// let percentage = 20;
+// let result;
+if (toldJoke === true) {
+    console.log($punchline);
+} else {
+    console.log(joke);
+}
+// Movie Night
+let zipCode = 98104;
+let joe = 'sFavoriteMovie = Forbidden Planet';
+let movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+    console.log("Too much!");
+} else {
+    console.log('We\'re going to see ' + 'joe' + '\'sFavoriteMovie');
+}
